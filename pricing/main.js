f149c43c695a73d7cac2dbb7f6080326878d6e53
Heisemberg <12972585+heisemberg@users.noreply.github.com>
2023-08-24 13:56:35 -0500

    seccion 2

--- pricing/main.js
+++ pricing/main.js
@@ -24,7 +24,33 @@ let construirElEncabezado = async()=>{
             <li>${value.imagen}${value.texto}</li>
             `
         ).join("")}
-    </ul>`);
+    </ul>
+    `);
+
+    let selecion2 = document.querySelector("#myJsonSection2");
+    selecion2.insertAdjacentHTML("beforeend", /*html*/`
+    ${res.section2.planes.map((value2)=> /*html*/ `
+    <div class="col">
+          <div class="card mb-4 rounded-0 border-primary">
+            <div class="card-header py-3 text-bg-primary border-primary  rounded-0">
+              <h4 class="my-0 fw-normal">${value2.titulo}</h4>
+            </div>
+            <div class="card-body">
+              <h1 class="card-title pricing-card-title">${value2.precio}<small class="text-body-secondary fw-light">/mo</small></h1>
+              <ul class="list-unstyled mt-3 mb-4">
+                <li>${value2.descripcion[0]}</li>
+                <li>${value2.descripcion[1]}</li>
+              </ul>
+              <button type="button" class="w-100 btn btn-lg btn-primary">Next</button>
+            </div>
+        </div>
+    </div>
+
+        `
+        ).join("")}
+    `);
+    
 }
 
 construirElEncabezado();
+
